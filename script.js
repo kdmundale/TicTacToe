ff5d5af7316735866e74995b617d0cbaf26140fe
Katherine Mundale <katherine.doris.mundale@gmail.com>
2019-11-26 13:31:33 -0500

    created every other player script

--- script.js
+++ script.js
@@ -1,42 +1,20 @@
-/*const grid = document.getElementsByTagName('grid');
-grid.addEventListener('click', event => {
-  let player = document.createTextNode('X');
-  grid.appendChild(player);
-*/
 
-
+let count=0;
 document.body.addEventListener('click', event => {
-  if (event.target.nodeName =='P'|| event.target.nodeName == 'DIV') {
-    player = event.target.textContent = 'O';
-    poop = getElementsByTagName('p');
-    let x = grid.replaceChildren(player);
-  }
+    if (count % 2 == 0 && event.target.nodeName =='P'|| event.target.nodeName == 'DIV') {
+        player = event.target.textContent = 'O';
+    } else if (count % 2 == 1 && event.target.nodeName =='P'|| event.target.nodeName == 'DIV') {
+      player = event.target.textContent = 'X';
+    }
+    count++;
 });
 
+/*
 document.body.addEventListener('dblclick', event => {
   if (event.target.nodeName =='P' || event.target.nodeName == 'DIV') {
     player = event.target.textContent = 'X';
     poop = getElementsByTagName('p');
     let x = grid.replaceChildren(player);
   }
-});
-
-
-
-
-
-
-
-/*let grid = document.getElementById('one');
-
-grid.addEventListener('dblclick', event => {
-  let player = document.createTextNode('o');
-  grid.appendChild(player);
-});
-
-grid.addEventListener('click', event => {
-  let poop = document.getElementById('poop1');
-  let player = document.createTextNode('X');
-  let x = grid.replaceChild(player, poop);
 });
 */

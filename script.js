10bce40ebfe24a7ab0a73cdd6f6f7d1fc7733ff4
Katherine Mundale <katherine.doris.mundale@gmail.com>
2019-11-26 12:26:38 -0500

    finished

--- script.js
+++ script.js
@@ -4,12 +4,38 @@ grid.addEventListener('click', event => {
   grid.appendChild(player);
 */
 
-let grid = document.getElementById('one');
-grid.addEventListener('click', event => {
-  let player = document.createTextNode('x');
-  grid.appendChild(player);
+
+document.body.addEventListener('click', event => {
+  if (event.target.nodeName =='P'|| event.target.nodeName == 'DIV') {
+    player = event.target.textContent = 'O';
+    poop = getElementsByTagName('p');
+    let x = grid.replaceChildren(player)
+  }
 });
+
+document.body.addEventListener('dblclick', event => {
+  if (event.target.nodeName =='P' || event.target.nodeName == 'DIV') {
+    player = event.target.textContent = 'X';
+    poop = getElementsByTagName('p');
+    let x = grid.replaceChildren(player)
+  }
+});
+
+
+
+
+
+
+/*let grid = document.getElementById('one');
+
 grid.addEventListener('dblclick', event => {
   let player = document.createTextNode('o');
   grid.appendChild(player);
 });
+
+grid.addEventListener('click', event => {
+  let poop = document.getElementById('poop1');
+  let player = document.createTextNode('X');
+  let x = grid.replaceChild(player, poop);
+});
+*/
